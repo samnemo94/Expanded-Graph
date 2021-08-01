05d2c963ad9b6a86ff697738e1711d5dcedb6dfa
Samy <samy.alnaimy@tatweer.sy>
2021-08-01 23:48:07 +0300

    Move util to utils.py

--- src/CENALP.py
+++ src/CENALP.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-import pickle as pkl
-import networkx as nx
-import numpy as np
-import pandas as pd
 from utils import *
 from walks import multi_simulate_walks, single_simulate_walks
 from sklearn.metrics.pairwise import cosine_similarity
 import warnings
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
-from sklearn.linear_model import LogisticRegression
 
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
@@ -143,8 +138,8 @@ def CENALP(G1, G2, q, attr1, attr2, attribute, alignment_dict, alignment_dict_re
             except:
                 continue
         print('All seed accuracy : %.2f%%'%(100 * count / len(seed_list1)))
-        pred1, pred2 = seed_link_lr(model, G1, G2, seed_list1, seed_list2, 
-                                mul, test_edges_final1, test_edges_final2, alignment_dict, alignment_dict_reversed)
+        pred1, pred2 = seed_link_lr(model, G1, G2, seed_list1, seed_list2,
+                                    mul, test_edges_final1, test_edges_final2, alignment_dict, alignment_dict_reversed)
 
         G1.add_edges_from(pred1)
         G2.add_edges_from(pred2)
@@ -174,186 +169,3 @@ def CENALP(G1, G2, q, attr1, attr2, attribute, alignment_dict, alignment_dict_re
     S = cosine_similarity(embedding2, embedding1)
     return S, precision, seed_l1, seed_l2
 
-def cal_degree_dict(G_list, G, layer):
-    G_degree = G.degree()
-    degree_dict = {}
-    degree_dict[0] = {}
-    for node in G_list:
-        degree_dict[0][node] = {node}
-    for i in range(1, layer + 1):
-        degree_dict[i] = {}
-        for node in G_list:
-            neighbor_set = []
-            for neighbor in degree_dict[i - 1][node]:
-                neighbor_set += nx.neighbors(G, neighbor)
-            neighbor_set = set(neighbor_set)
-            for j in range(i - 1, -1, -1):
-                neighbor_set -= degree_dict[j][node]
-            degree_dict[i][node] = neighbor_set
-    for i in range(layer + 1):
-        for node in G_list:
-            if len(degree_dict[i][node]) == 0:
-                degree_dict[i][node] = [0]
-            else:
-                degree_dict[i][node] = node_to_degree(G_degree, degree_dict[i][node])
-    return degree_dict
-
-def seed_link(seed_list1, seed_list2, G1, G2, anchor):
-    k = 0
-    for i in range(len(seed_list1) - 1):
-        for j in range(np.max([anchor + 1, i + 1]), len(seed_list1)):
-            if G1.has_edge(seed_list1[i], seed_list1[j]) and not G2.has_edge(seed_list2[i], seed_list2[j]):
-                G2.add_edges_from([[seed_list2[i], seed_list2[j]]])
-                k += 1
-            if not G1.has_edge(seed_list1[i], seed_list1[j]) and G2.has_edge(seed_list2[i], seed_list2[j]):
-                G1.add_edges_from([[seed_list1[i], seed_list1[j]]])
-                k += 1
-    print('Add seed links : {}'.format(k), end = '\t')
-    return k
-def node_to_degree(G_degree, SET):
-    SET = list(SET)
-    SET = sorted([G_degree[x] for x in SET])
-    return SET
-    
-def caculate_jaccard_coefficient(G1, G2, seed_list1, seed_list2, index, columns, alignment_dict = None):
-    mul = int(np.max([np.max(G1.nodes()), np.max(G2.nodes())]))
-    seed1_dict = {}
-    seed1_dict_reversed = {}
-    seed2_dict = {}
-    seed2_dict_reversed = {}
-    for i in range(len(seed_list1)):
-        seed1_dict[i + 2 * (mul + 1)] = seed_list1[i]
-        seed1_dict_reversed[seed_list1[i]] = i + 2 * (mul + 1)
-        seed2_dict[i + 2 * (mul + 1)] = seed_list2[i] + mul + 1
-        seed2_dict_reversed[seed_list2[i] + mul + 1] = i + 2 * (mul + 1)
-    G1_edges = pd.DataFrame(G1.edges())
-    G1_edges.iloc[:, 0] = G1_edges.iloc[:, 0].apply(lambda x:to_seed(x, seed1_dict_reversed))
-    G1_edges.iloc[:, 1] = G1_edges.iloc[:, 1].apply(lambda x:to_seed(x, seed1_dict_reversed))
-    G1_edges.iloc[:, 0] = G1_edges.iloc[:, 0].apply(lambda x:to_seed(x, seed1_dict_reversed))
-    G2_edges = pd.DataFrame(G2.edges())
-    G2_edges += mul + 1
-    G2_edges.iloc[:, 0] = G2_edges.iloc[:, 0].apply(lambda x:to_seed(x, seed2_dict_reversed))
-    G2_edges.iloc[:, 1] = G2_edges.iloc[:, 1].apply(lambda x:to_seed(x, seed2_dict_reversed))
-    adj = nx.Graph()
-    adj.add_edges_from(np.array(G1_edges))
-    adj.add_edges_from(np.array(G2_edges))
-    jaccard_dict = {}
-    for G1_node in index:
-        for G2_node in columns:
-            if (G1_node, G2_node) not in jaccard_dict.keys():
-                jaccard_dict[G1_node, G2_node] = 0
-            jaccard_dict[G1_node, G2_node] += calculate_adj(adj.neighbors(G1_node), adj.neighbors(G2_node + mul + 1))
-    
-    jaccard_dict = [[x[0][0], x[0][1], x[1]] for x in jaccard_dict.items()]
-    adj_matrix = np.array(jaccard_dict)
-    return adj_matrix
-def flatten(input_list):
-    output_list = []
-    while True:
-        if input_list == []:
-            break
-        for index, i in enumerate(input_list):
-
-            if type(i)== list:
-                input_list = i + input_list[index+1:]
-                break
-            else:
-                output_list.append(i)
-                input_list.pop(index)
-                break
-
-    return output_list
-def clip(x):
-    if x <= 0:
-        return 0
-    else:
-        return x
-def calculate_adj(setA, setB):
-    setA = set(setA)
-    setB = set(setB)
-    ep = 0.5
-    inter = len(setA & setB) + ep
-    union = len(setA | setB) + ep
-
-    adj = inter / union
-    return adj
-def to_seed(x, dictionary):
-    try:
-        return dictionary[x]
-    except:
-        return x
-def edge_sample(G):
-    edges = list(G.edges())
-    test_edges_false = []
-    while len(test_edges_false) < G.number_of_edges():
-        node1 = np.random.choice(G.nodes())
-        node2 = np.random.choice(G.nodes())
-        if node1 == node2:
-            continue
-        if G.has_edge(node1, node2):
-            continue
-        test_edges_false.append([min(node1, node2), max(node1, node2)])
-    edges = edges + test_edges_false
-    return edges
-def seed_link_lr(model, G1, G2, seed_list1, seed_list2, mul, test_edges_final1, test_edges_final2, alignment_dict, alignment_dict_reversed):
-
-    train_edges_G1 = edge_sample(G1)
-    embedding1 = [np.concatenate([model.wv[str(edge[0])], model.wv[str(edge[1])], 
-                model.wv[str(edge[0])] * model.wv[str(edge[1])]]) for edge in train_edges_G1]
-    label1 = [1] * G1.number_of_edges() + [0] * (len(train_edges_G1) - G1.number_of_edges())
-    
-    train_edges_G2 = edge_sample(G2)
-    embedding2 = [np.concatenate([model.wv[str(edge[0] + mul + 1)], model.wv[str(edge[1] + mul + 1)], 
-                model.wv[str(edge[0] + mul + 1)] * model.wv[str(edge[1] + mul + 1)]]) for edge in train_edges_G2]
-    label2 = [1] * G2.number_of_edges() + [0] * (len(train_edges_G2) - G2.number_of_edges())
-    
-    embedding = embedding1 + embedding2
-    label = label1 + label2
-
-    edge_classifier = LogisticRegression(solver = 'liblinear', random_state=0)
-    edge_classifier.fit(np.array(embedding), label)
-
-    test_edges1 = []
-    test_edges2 = []
-    for i in range(len(seed_list1)):
-        for j in range(i+1, len(seed_list1)):
-            if not G1.has_edge(seed_list1[i], seed_list1[j]) and G2.has_edge(seed_list2[i], seed_list2[j]):
-                test_edges1.append([min(seed_list1[i], seed_list1[j]), max(seed_list1[i], seed_list1[j])])
-            if not G2.has_edge(seed_list2[i], seed_list2[j]) and G1.has_edge(seed_list1[i], seed_list1[j]):
-                test_edges2.append([min(seed_list2[i], seed_list2[j]), max(seed_list2[i], seed_list2[j])])
-    test_edges1, test_edges2 = np.array(test_edges1), np.array(test_edges2)
-    embedding1 = [np.concatenate([model.wv[str(edge[0])], model.wv[str(edge[1])], 
-                model.wv[str(edge[0])] * model.wv[str(edge[1])]]) for edge in test_edges1]
-    embedding2 = [np.concatenate([model.wv[str(edge[0] + mul + 1)], model.wv[str(edge[1] + mul + 1)], 
-                model.wv[str(edge[0] + mul + 1)] * model.wv[str(edge[1] + mul + 1)]]) for edge in test_edges2]
-    if len(embedding1) != 0:
-        val_preds1 = edge_classifier.predict_proba(embedding1)[:, 1]
-        
-        pred1 = test_edges1[val_preds1>0.5]
-    else:
-        pred1 = []
-    if len(embedding2) != 0:
-        val_preds2 = edge_classifier.predict_proba(embedding2)[:, 1]
-        pred2 = test_edges2[val_preds2>0.5]
-    else:
-        pred2 = []
-    '''
-    pred1 = [(alignment_dict[edge[0]], alignment_dict[edge[1]]) for edge in pred1]
-    pred2 = [(alignment_dict_reversed[edge[0]], alignment_dict_reversed[edge[1]]) for edge in pred2]
-    if len(test_edges_final1) != 0:
-        test_edges_final1 = [(min([x[0], x[1]]), max([x[0], x[1]])) for x in test_edges_final1]
-        #test_edges_final1 = [str(x) for x in test_edges_final1]
-        precision1 = 100 * len(np.intersect1d(test_edges_final1, pred2))/len(test_edges_final1)
-    else:
-        precision1 = 0
-    if len(test_edges_final2) != 0:
-        test_edges_final2 = [(min([x[0], x[1]]), max([x[0], x[1]])) for x in test_edges_final2]
-        #test_edges_final2 = [str(x) for x in test_edges_final2]
-        precision2 = 100 * len(np.intersect1d(test_edges_final2, pred1))/len(test_edges_final2)
-    else:
-        precision2 = 0
-    pred1 = [(alignment_dict_reversed[edge[0]], alignment_dict_reversed[edge[1]]) for edge in pred1]
-    pred2 = [(alignment_dict[edge[0]], alignment_dict[edge[1]]) for edge in pred2]
-    '''
-    return pred1, pred2
-    
