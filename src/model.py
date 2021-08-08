e5f23b44923cc15ff810eee10a19ce5801d66f80
Samy <samy.alnaimy@tatweer.sy>
2021-08-08 13:59:03 +0300

    Simplify Equation

--- src/model.py
+++ src/model.py
@@ -6,28 +6,27 @@ def structing(layers, G1, G2, G1_degree_dict, G2_degree_dict, attribute, alpha, 
     G2_nodes = list(G2.nodes())
 
     k1 = k2 = 1
-    pp_dist_matrix = {}
-    pp_dist_df = pd.DataFrame(np.zeros((G1.number_of_nodes(), G2.number_of_nodes())), 
+    pp_dist_df = pd.DataFrame(np.zeros((G1.number_of_nodes(), G2.number_of_nodes())),
                                        index=G1_nodes, columns=G2_nodes)
     
     for layer in range(layers + 1):
 
         L1 = [np.log(k1 * np.max(G1_degree_dict[layer][x]) + np.e) for x in G1_nodes]
         L2 = [np.log(k2 * np.max(G2_degree_dict[layer][x]) + np.e) for x in G2_nodes]
-        pp_dist_matrix[layer, 0] = pd.DataFrame(np.transpose(np.array(L1 * G2.number_of_nodes()).reshape(-1, G1.number_of_nodes())), 
+        pp_dist_matrix1 = pd.DataFrame(np.transpose(np.array(L1 * G2.number_of_nodes()).reshape(-1, G1.number_of_nodes())),
                           index=G1_nodes, columns=G2_nodes)
-        pp_dist_matrix[layer, 1] = pd.DataFrame(np.array(list(L2 * G1.number_of_nodes())).reshape(-1, G2.number_of_nodes()), 
+        pp_dist_matrix2 = pd.DataFrame(np.array(list(L2 * G1.number_of_nodes())).reshape(-1, G2.number_of_nodes()),
                           index=G1_nodes, columns=G2_nodes)
-        pp_dist_df += abs(pp_dist_matrix[layer, 0] - pp_dist_matrix[layer, 1])
+        pp_dist_df += abs(pp_dist_matrix1 - pp_dist_matrix2)
     for layer in range(layers + 1):
 
         L1 = [np.log(k1 * np.min(G1_degree_dict[layer][x]) + 1) for x in G1_nodes]
         L2 = [np.log(k2 * np.min(G2_degree_dict[layer][x]) + 1) for x in G2_nodes]
-        pp_dist_matrix[layer, 0] = pd.DataFrame(np.transpose(np.array(L1 * G2.number_of_nodes()).reshape(-1, G1.number_of_nodes())), 
+        pp_dist_matrix1 = pd.DataFrame(np.transpose(np.array(L1 * G2.number_of_nodes()).reshape(-1, G1.number_of_nodes())),
                           index=G1_nodes, columns=G2_nodes)
-        pp_dist_matrix[layer, 1] = pd.DataFrame(np.array(list(L2 * G1.number_of_nodes())).reshape(-1, G2.number_of_nodes()), 
+        pp_dist_matrix2 = pd.DataFrame(np.array(list(L2 * G1.number_of_nodes())).reshape(-1, G2.number_of_nodes()),
                           index=G1_nodes, columns=G2_nodes)
-        pp_dist_df += abs(pp_dist_matrix[layer, 0] - pp_dist_matrix[layer, 1])
+        pp_dist_df += abs(pp_dist_matrix1 - pp_dist_matrix2)
     pp_dist_df /= 2
     pp_dist_df = np.exp(-alpha * pp_dist_df)
     if len(attribute) != 0:
